66e31fe32f8aab2a218ac8498081f51f1bc369b3
zhensama-1 <zhensama05@gmail.com>
2026-07-28 23:44:53 +0800

    Add procedural modeling package exports

---
+++ src/procedural_modeling/__init__.py
@@ -0,0 +1,5 @@
+from .fitting import fit_dimensions_to_bbox
+from .materials import infer_material_for_category
+from .specs import procedural_spec_for_category
+
+__all__ = ["fit_dimensions_to_bbox", "infer_material_for_category", "procedural_spec_for_category"]
