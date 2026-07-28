92138c988d3fcb4b1ef23f99dce089dcf12c0708
zhensama-1 <zhensama05@gmail.com>
2026-07-28 23:45:05 +0800

    Add scene initialization package exports

---
+++ src/scene_initialization/__init__.py
@@ -0,0 +1,4 @@
+from .initializer import SceneInitializer
+from .types import DetectedObject, SceneInitializationObject
+
+__all__ = ["DetectedObject", "SceneInitializationObject", "SceneInitializer"]
